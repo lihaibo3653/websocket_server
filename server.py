3944f1c4e9076f64bcd3f9137e17a89483d8fb40
lihaibo3653 <lihaibo3653@dingtalk.com>
2018-11-11 14:24:00 +0800

    add file

---
+++ server.py
@@ -0,0 +1,30 @@
+import tornado
+import tornado.ioloop
+import tornado.websocket
+
+class WebSocket(tornado.websocket.WebSocketHandler):
+
+    def open(self):
+        print('on websoket open')
+
+    def on_message(self,message):
+        self.write_message("I have receive your message:",message)
+
+    def on_close(self):
+        print("on websocket close")
+
+    def check_orgin(self,orgin):
+        return True
+
+
+patterns = [
+    (r"/ws",WebSocket)
+]
+
+settings = {
+    "debug":True
+}
+app = tornado.web.Application(patterns,**settings);
+app.listern(7000,xheaders = True)
+
+tornado.ioloop.IOLoop.current().start()
